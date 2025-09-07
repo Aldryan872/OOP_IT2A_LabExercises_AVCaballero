ef1531081403cebca71e638c62627294b637e3df
Aldryan872 <toritacaballero@gmail.com>
2025-09-07 14:55:15 +0800

    Update Exer1_Declarative.java
    Signed-off-by: Aldryan872 <toritacaballero@gmail.com>

--- EXER1_Caballero/Exer1_Declarative.java
+++ EXER1_Caballero/Exer1_Declarative.java
@@ -2,7 +2,7 @@
 // using declarative style of coding
 import java.util.Arrays;
 import java.util.List;
-public class GFG {
+public class main {
     public static void main(String[] args)
     {
         List<Integer> numbers
