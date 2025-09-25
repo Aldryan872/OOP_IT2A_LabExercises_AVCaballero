a7a16ae8445c936a5a581ff5fb76ac3beffb4d54
Aldryan Venth Caballero <toritacaballero@gmail.com>
2025-09-25 18:10:49 +0800

    Update and rename ProjectTester.java to BaseClass.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

---
+++ EXER3-B/BaseClass.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class HealthCenterDashboard extends JFrame {
+    public HealthCenterDashboard() {
+        setTitle("Health Center Dashboard");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        System.out.println("Health Center Dashboard initialized");
+    }
+}
