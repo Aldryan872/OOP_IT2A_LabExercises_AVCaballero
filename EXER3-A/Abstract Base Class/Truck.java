13cca09f126c042d932363bdea014b2647739c1e
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:33:28 +0800

    Update Truck.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER3-A/Abstract Base Class/Truck.java
+++ EXER3-A/Abstract Base Class/Truck.java
@@ -1,28 +1,6 @@
+// Truck.java
 public class Truck extends LandTransport {
-    private double cargoCapacity;
-
-    public Truck(String name, int maxSpeed, int capacity, int wheelCount, double cargoCapacity) {
-        super(name, maxSpeed, capacity, wheelCount);
-        this.cargoCapacity = cargoCapacity;
-    }
-
-    @Override
-    public void move() {
-        System.out.println(name + " is transporting goods on the road");
-    }
-
-    @Override
-    public void drive() {
-        System.out.println(name + " is being driven on highways");
-    }
-
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Cargo Capacity: " + cargoCapacity + " tons");
-    }
-
-    public void loadCargo() {
-        System.out.println(name + " is loading cargo");
+    public Truck(int capacity) {
+        super("Truck", capacity);
     }
 }
