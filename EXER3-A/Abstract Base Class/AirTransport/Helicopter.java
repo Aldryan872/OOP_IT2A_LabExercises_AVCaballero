e5787b7175ad11a360167b565606a73efd12b684
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:35:18 +0800

    Update Helicopter.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER3-A/Abstract Base Class/AirTransport/Helicopter.java
+++ EXER3-A/Abstract Base Class/AirTransport/Helicopter.java
@@ -1,28 +1,6 @@
+// Helicopter.java
 public class Helicopter extends AirTransport {
-    private int rotorCount;
-
-    public Helicopter(String name, int maxSpeed, int capacity, int maxAltitude, int rotorCount) {
-        super(name, maxSpeed, capacity, maxAltitude);
-        this.rotorCount = rotorCount;
-    }
-
-    @Override
-    public void move() {
-        System.out.println(name + " is flying using rotors");
-    }
-
-    @Override
-    public void takeOff() {
-        System.out.println(name + " is taking off vertically");
-    }
-
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Rotor Count: " + rotorCount);
-    }
-
-    public void hover() {
-        System.out.println(name + " is hovering in place");
+    public Helicopter(int capacity) {
+        super("Helicopter", capacity);
     }
 }
