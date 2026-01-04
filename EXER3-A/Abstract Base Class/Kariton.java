465f464e5c3f6c0cfdfec75e8db6fbdcdf157da6
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:15:46 +0800

    Update Kariton.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER3-A/Abstract Base Class/Kariton.java
+++ EXER3-A/Abstract Base Class/Kariton.java
@@ -1,28 +1,6 @@
+// Kariton.java
 public class Kariton extends LandTransport {
-    private String material;
-
-    public Kariton(String name, int maxSpeed, int capacity, int wheelCount, String material) {
-        super(name, maxSpeed, capacity, wheelCount);
-        this.material = material;
-    }
-
-    @Override
-    public void move() {
-        System.out.println(name + " is being pushed or pulled manually");
-    }
-
-    @Override
-    public void drive() {
-        System.out.println(name + " is being moved by human power");
-    }
-
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Material: " + material);
-    }
-
-    public void carryGoods() {
-        System.out.println(name + " is carrying various goods");
+    public Kariton(int capacity) {
+        super("Kariton", capacity);
     }
 }
