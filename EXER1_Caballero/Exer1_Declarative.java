de3dde7fde82896e599dfd2a0f75035262928717
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:06:22 +0800

    Update Exer1_Declarative.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER1_Caballero/Exer1_Declarative.java
+++ EXER1_Caballero/Exer1_Declarative.java
@@ -1,19 +1,12 @@
-// Java program to find the sum
-// using declarative style of coding
-import java.util.Arrays;
 import java.util.List;
-public class main {
-    public static void main(String[] args)
-    {
-        List<Integer> numbers
-            = Arrays.asList(11, 22, 33, 44,
-                            55, 66, 77, 88,
-                            99, 100);
 
-        System.out.println(
-            numbers.stream()
-                .filter(number -> number % 2 == 0)
-                .mapToInt(e -> e * 2)
-                .sum());
+public class EXER1_Declarative {
+    public static void main(String[] args) {
+        List<String> names = List.of("Aldryan", "Robert", "Rodrigo");
+
+        // declarative: say what we want, not how
+        names.stream()
+             .filter(name -> name.startsWith("A"))
+             .forEach(System.out::println);
     }
 }
