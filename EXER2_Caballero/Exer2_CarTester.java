15c058a918cfcdfbd74b895ad7e49cc78ba736f2
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:11:13 +0800

    Update Exer2_CarTester.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER2_Caballero/Exer2_CarTester.java
+++ EXER2_Caballero/Exer2_CarTester.java
@@ -1,58 +1,39 @@
-public class CarTester {
+package cartest;
 
+import car.Car;
+
+public class CarTester {
     public static void main(String[] args) {
-        // Create an array of 10 Car objects with different brands
-        Car[] cars = new Car[10];
-        
-        // Initialize car objects with brand information in the plateNo field
-        // Using plateNo to store brand name as per your requirement
-        cars[0] = new Car("Red", "Toyota (Founded 1937 by Kiichiro Toyoda)", "CH001");
-        cars[1] = new Car("Blue", "Ford (Founded 1903 by Henry Ford)", "CH002");
-        cars[2] = new Car("Black", "Mercedes-Benz (Founded 1926 by Karl Benz)", "CH003");
-        cars[3] = new Car("White", "BMW (Founded 1916 by Karl Rapp)", "CH004");
-        cars[4] = new Car("Silver", "Honda (Founded 1948 by Soichiro Honda)", "CH005");
-        cars[5] = new Car("Green", "Volkswagen (Founded 1937 by German Labor Front)", "CH006");
-        cars[6] = new Car("Yellow", "Ferrari (Founded 1939 by Enzo Ferrari)", "CH007");
-        cars[7] = new Car("Gray", "Tesla (Founded 2003 by Martin Eberhard & Marc Tarpenning)", "CH008");
-        cars[8] = new Car("Orange", "Porsche (Founded 1931 by Ferdinand Porsche)", "CH009");
-        cars[9] = new Car("Purple", "Nissan (Founded 1933 by Yoshisuke Aikawa)", "CH010");
-        
-        // Display information for all cars
-        System.out.println("=== CAR BRANDS COLLECTION ===");
-        System.out.println("==============================");
-        
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println("\nCar #" + (i + 1) + ":");
-            cars[i].displayInfo();
-            System.out.println("------------------------------");
-        }
-        
-        // Alternative: Display with more detailed brand information
-        System.out.println("\n\n=== DETAILED CAR BRANDS INFORMATION ===");
-        System.out.println("========================================");
-        
-        // Create a more detailed display using the existing Car class structure
-        String[] brands = {
-            "Toyota - Founded: 1937, Founder: Kiichiro Toyoda",
-            "Ford - Founded: 1903, Founder: Henry Ford",
-            "Mercedes-Benz - Founded: 1926, Founder: Karl Benz",
-            "BMW - Founded: 1916, Founder: Karl Rapp",
-            "Honda - Founded: 1948, Founder: Soichiro Honda",
-            "Volkswagen - Founded: 1937, Founder: German Labor Front",
-            "Ferrari - Founded: 1939, Founder: Enzo Ferrari",
-            "Tesla - Founded: 2003, Founders: Martin Eberhard & Marc Tarpenning",
-            "Porsche - Founded: 1931, Founder: Ferdinand Porsche",
-            "Nissan - Founded: 1933, Founder: Yoshisuke Aikawa"
-        };
-        
-        String[] colors = {"Red", "Blue", "Black", "White", "Silver", 
-                          "Green", "Yellow", "Gray", "Orange", "Purple"};
-        
-        for (int i = 0; i < 10; i++) {
-            Car car = new Car(colors[i], brands[i], "CH" + String.format("%03d", i+1));
-            System.out.println("\nBrand " + (i + 1) + ":");
-            car.displayInfo();
-            System.out.println("----------------------------");
-        }
+        Car c1 = new Car("Toyota", "Yellow", "KAW12345", "AB123", "Gasoline", 180, 2020, 25000);
+        Car c2 = new Car("Honda", "Red", "SYA7890", "CD456", "Diesel", 200, 2019, 22000);
+        Car c3 = new Car("Ford", "Blue", "AKO081206", "EF789", "Gasoline", 280, 2021, 30000);
+        Car c4 = new Car("Rolls Royce", "Black", "ANAKO123", "7", "PULA", 250, 2022, 55000);
+        Car c5 = new Car("Mercedes", "White", "LAO4546", "IJ345", "GREEN", 300, 2021, 60000);
+        Car c6 = new Car("TOYOTA MK4 SUPRA", "Silver", "OPAW321", "KL678", "Green", 399, 2018, 20000);
+        Car c7 = new Car("Mclarren", "Black", "WEW53576", "DISCAYA69", "Crudo", 280, 2016, 35000);
+        Car c8 = new Car("Suzuki Mini Van", "Purple", "YNW6769", "OP234", "Diesel", 330, 2010, 65000);
+        Car c9 = new Car("FORD", "Black", "AKP123", "QRY260", "Special", 210, 2020, 28000);
+        Car c10 = new Car("Toyota AE86", "white", "JPN7", "JPN7", "Special", 400, 2014, 27000);
+
+        c1.displayInfo();
+                System.out.println("-----------------------------------");
+        c2.displayInfo();
+                System.out.println("-----------------------------------");
+        c3.displayInfo();
+                System.out.println("-----------------------------------");
+        c4.displayInfo();
+                System.out.println("-----------------------------------");
+        c5.displayInfo();
+                System.out.println("-----------------------------------");
+        c6.displayInfo();
+        System.out.println("-----------------------------------");
+        c7.displayInfo();
+        System.out.println("-----------------------------------");
+        c8.displayInfo();
+        System.out.println("-----------------------------------");
+        c9.displayInfo();
+        System.out.println("-----------------------------------");
+        c10.displayInfo();
+        System.out.println("-----------------------------------");
     }
 }
