236299fd7a74c68be56051931738ad026e0659ba
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:32:59 +0800

    Simplify Tricycle class constructor and methods
    Refactored Tricycle class to simplify constructor and removed unused methods.
    
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER3-A/Abstract Base Class/Tricycle.java
+++ EXER3-A/Abstract Base Class/Tricycle.java
@@ -1,28 +1,6 @@
+// Tricycle.java
 public class Tricycle extends LandTransport {
-    private boolean hasBasket;
-
-    public Tricycle(String name, int maxSpeed, int capacity, int wheelCount, boolean hasBasket) {
-        super(name, maxSpeed, capacity, wheelCount);
-        this.hasBasket = hasBasket;
-    }
-
-    @Override
-    public void move() {
-        System.out.println(name + " is pedaling on streets");
-    }
-
-    @Override
-    public void drive() {
-        System.out.println(name + " is being pedaled manually");
-    }
-
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Has Basket: " + (hasBasket ? "Yes" : "No"));
-    }
-
-    public void ringBell() {
-        System.out.println(name + " is ringing its bell");
+    public Tricycle(int capacity) {
+        super("Tricycle", capacity);
     }
 }
