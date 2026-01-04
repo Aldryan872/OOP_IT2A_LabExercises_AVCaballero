498cd9383b2d4ef5e3f8b6bde20d08cb93439f4b
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:31:50 +0800

    Update SUV.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER3-A/Abstract Base Class/SUV.java
+++ EXER3-A/Abstract Base Class/SUV.java
@@ -1,24 +1,6 @@
+// SUV.java
 public class SUV extends LandTransport {
-    private boolean fourWheelDrive;
-
-    public SUV(String name, int maxSpeed, int capacity, int wheelCount, boolean fourWheelDrive) {
-        super(name, maxSpeed, capacity, wheelCount);
-        this.fourWheelDrive = fourWheelDrive;
-    }
-
-    @Override
-    public void move() {
-        System.out.println(name + " is driving on various terrains");
-    }
-
-    @Override
-    public void drive() {
-        System.out.println(name + " is being driven as a family vehicle");
-    }
-
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("4WD: " + (fourWheelDrive ? "Yes" : "No"));
+    public SUV(int capacity) {
+        super("SUV", capacity);
     }
 }
