fe0f7bd04b277db24fe9de4746c168a4f110aae7
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-04 12:34:29 +0800

    Update Transportation.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER3-A/Transportation.java
+++ EXER3-A/Transportation.java
@@ -1,34 +1,22 @@
-// TransportationTester.java
-public class TransportationTester {
-    public static void main(String[] args) {
-        // Air Transport
-        Helicopter helicopter = new Helicopter(5);
-        Airplane airplane = new Airplane(150);
-        SpaceShuttle shuttle = new SpaceShuttle(7);
+// Transportation.java
+public class Transportation {
+    private String name;
+    private int capacity;
 
-        // Land Transport
-        Truck truck = new Truck(3);
-        SUV suv = new SUV(7);
-        Tricycle tricycle = new Tricycle(2);
-        Motorcycle motorcycle = new Motorcycle(2);
-        Kariton kariton = new Kariton(1);
+    public Transportation(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
 
-        // Water Transport
-        Boat boat = new Boat(20);
-        Submarine submarine = new Submarine(30);
+    public void move() {
+        System.out.println(name + " is moving...");
+    }
 
-        // Testing move() method
-        helicopter.move();
-        airplane.move();
-        shuttle.move();
+    public String getName() {
+        return name;
+    }
 
-        truck.move();
-        suv.move();
-        tricycle.move();
-        motorcycle.move();
-        kariton.move();
-
-        boat.move();
-        submarine.move();
+    public int getCapacity() {
+        return capacity;
     }
 }
