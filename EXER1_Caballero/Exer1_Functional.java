df7df02dd8cebb7741ad31d13532a7dd20818447
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-05 09:56:41 +0800

    Update Exer1_Functional.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER1_Caballero/Exer1_Functional.java
+++ EXER1_Caballero/Exer1_Functional.java
@@ -1,16 +1,22 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class FunctionalExample {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] numbers = IntStream.range(1, 101).toArray();
 
         // functional style using streams
-        int sum = Arrays.stream(numbers)
+        int sumEven = Arrays.stream(numbers)
+                        .filter(n -> n % 2 == 0) // keep even numbers
+                        .map(n -> n * n)         // square them
+                        .sum();
+
+        int sumOdd = Arrays.stream(numbers)
                         .filter(n -> n % 2 == 1) // keep odd numbers
                         .map(n -> n * n)         // square them
                         .sum();
 
-        System.out.println("Sum of squares of odd numbers = " + sumOdd);
         System.out.println("Sum of squares of even numbers = " + sumEven);
+        System.out.println("Sum of squares of odd numbers = " + sumOdd);
     }
 }
