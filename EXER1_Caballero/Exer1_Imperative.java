38222361040fcd90d14d559adf9c68503730e4fa
Aldryan Venth Caballero <toritacaballero@gmail.com>
2026-01-05 10:02:32 +0800

    Update Exer1_Imperative.java
    Signed-off-by: Aldryan Venth Caballero  <toritacaballero@gmail.com>

--- EXER1_Caballero/Exer1_Imperative.java
+++ EXER1_Caballero/Exer1_Imperative.java
@@ -1,13 +1,19 @@
-public class ImperativeExample {
+public class Imperative {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         int sum = 0;
+        int product = 1;
 
         // step-by-step instructions
         for (int num : numbers) {
             sum += num;
+            product *= num;
         }
 
+        double average = (double) sum / numbers.length;
+
         System.out.println("Sum = " + sum);
+        System.out.println("Product = " + product);
+        System.out.println("Average = " + average);
     }
 }
